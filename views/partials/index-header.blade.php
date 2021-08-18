35bc0dbb86eecb16b98b20688a719622da2e5cbb
Tomsci <62972629+itstomsci@users.noreply.github.com>
2021-08-18 20:50:18 +0100

    Updated headers & navigation partials

---
+++ views/partials/index-header.blade.php
@@ -0,0 +1,12 @@
+  <header class="mb-4">
+    <div class="content">
+      @include('partials.navigation')
+      <div class="d-flex justify-content-center m-4">
+        <div class="d-flex flex-column">
+          <img class="d-block mx-auto rounded-circle m-4" src="{{ $config->get('Domain') }}/public/media/logo.jpg">
+          <h1>{{ $config->get('App Name') }}</h1>
+          <p>A little description here.</p>
+        </div>
+      </div>
+    </div>
+  </header>
--- views/partials/navigation.blade.php
+++ views/partials/navigation.blade.php
@@ -1,97 +1,81 @@
-
-<div class="content">
     <nav class="navbar navbar-expand-lg navbar-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-                <img class="d-block mx-auto rounded-circle" src="/public/media/logo.jpg" alt="CloneWarsRP">
-            </a>
-            <button class="navbar-toggler" data-bs-target="#navbar-dropdown" data-bs-toggle="collapse" type="button"
-                    aria-controls="navbar-dropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbar-dropdown">
-                <ul class="navbar-nav me-auto">
-                    <li class="nav-item">
-                        <a class="nav-link active" href="#" aria-current="page">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Forums</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" id="navbar-community-dropdown-menu" data-bs-toggle="dropdown"
-                           role="button" aria-expanded="false" style="color: gold; font-weight: bold;">
-                            Store
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbar-community-dropdown-menu">
-                            <li>
-                                <a class="dropdown-item" href="#">Buy Credits</a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="#">Gift Credits</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" id="navbar-community-dropdown-menu" data-bs-toggle="dropdown"
-                           role="button" aria-expanded="false">
-                            Community
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbar-community-dropdown-menu">
-                            <li>
-                                <a class="dropdown-item" href="#">Users</a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="#">Leaderboard</a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="#">Staff</a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="#">Punishments</a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="#">Rules</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" id="navbar-community-dropdown-menu" data-bs-toggle="dropdown"
-                           role="button" aria-expanded="false">
-                            Social
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbar-community-dropdown-menu">
-                            <li>
-                                <a class="dropdown-item" href="#">Steam</a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="#">Discord</a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="#">Gametracker</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" id="navbar-community-dropdown-menu" data-bs-toggle="dropdown"
-                           role="button" aria-expanded="false">
-                            Other
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbar-community-dropdown-menu">
-                            <li>
-                                <a class="dropdown-item" href="#">Developer Logs</a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="#">Terms Of Service</a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="#">Privacy Policy</a>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-                <button class="btn btn-outline-light" type="submit">Login</button>
-            </div>
+      <div class="container-fluid">
+        <a class="navbar-brand">
+          <img class="d-block mx-auto rounded-circle" src="{{ $config->get('Domain') }}/public/media/logo.jpg">
+        </a>
+        <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbar-dropdown">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbar-dropdown">
+          <ul class="navbar-nav me-auto">
+            <li class="nav-item" href="{{ $config->get('Domain') }}">
+              <a class="nav-link active">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{ $config->get('Domain') }}/forums">Forums</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown"
+                style="color: gold; font-weight: bold;">Store</a>
+              <ul class="dropdown-menu">
+                <li>
+                  <a class="dropdown-item" onclick="buyCredits()">Buy Credits</a>
+                </li>
+                <li>
+                  <a class="dropdown-item" onclick="giftCredits()">Gift Credits</a>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Community</a>
+              <ul class="dropdown-menu">
+                <li>
+                  <a class="dropdown-item" href="{{ $config->get('Domain') }}/community/users">Users</a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="{{ $config->get('Domain') }}/community/leaderboard">Leaderboard</a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="{{ $config->get('Domain') }}/community/staff">Staff</a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="{{ $config->get('Domain') }}/community/punishments">Punishments</a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="{{ $config->get('Domain') }}/community/rules">Rules</a>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Social</a>
+              <ul class="dropdown-menu">
+                <li>
+                  <a class="dropdown-item" href="{{ $config->get('Domain') }}/social/steam">Steam</a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="{{ $config->get('Domain') }}/social/discord">Discord</a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="{{ $config->get('Domain') }}/social/gametracker">Gametracker</a>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Other</a>
+              <ul class="dropdown-menu">
+                <li>
+                  <a class="dropdown-item" href="{{ $config->get('Domain') }}/commits">Commits</a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="{{ $config->get('Domain') }}/legal/termsofservice">Terms Of Service</a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="{{ $config->get('Domain') }}/legal/privacypolicy">Privacy Policy</a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+          <button class="btn btn-outline-light" type="submit">Login</button>
         </div>
+      </div>
     </nav>
-    @yield('header')
-</div>
