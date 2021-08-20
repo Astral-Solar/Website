eb817fd848e4fe456ca46974d0bb8b7df348a63b
Tomsci <62972629+itstomsci@users.noreply.github.com>
2021-08-20 17:52:25 +0100

    Updated index page

--- views/page/index.blade.php
+++ views/page/index.blade.php
@@ -1,54 +1,72 @@
 @extends('layouts.app')
 
 @section('title', 'Home')
-
-@section('content')
-    <div class="row d-flex justify-content-center">
-        <div class="col-lg-4 mb-4">
-            <div class="card text-center">
-                <div class="card-body" style="background-color: #0f1010; color: #ffffff">
-                    <h1 class="card-title">
-                        0
-                    </h1>
-                    <p class="card-text">
-                        Online Members
-                    </p>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-4 mb-4">
-            <div class="card text-center">
-                <div class="card-body" style="background-color: #0f1010; color: #ffffff">
-                    <h1 class="card-title">
-                        0
-                    </h1>
-                    <p class="card-text">
-                        Total Members
-                    </p>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-4 mb-4">
-            <div class="card text-center">
-                <div class="card-body" style="background-color: #0f1010; color: #ffffff">
-                    <h1 class="card-title">
-                        1
-                    </h1>
-                    <p class="card-text">
-                        Total Discord Members
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
-@endsection
+@section('css', '/public/css/index.css')
 
 @section('header')
-    <div class="d-flex justify-content-center m-4">
-        <div class="d-flex flex-column">
-            <img class="d-block mx-auto rounded-circle m-4" src="public/media/logo.jpg">
-            <h1>CloneWarsRP</h1>
-            <p>A little description here.</p>
+@include('partials.index.header')
+@endsection
+
+@section('content')
+<section class="statistics">
+  <div class="d-flex justify-content-center m-2">
+    <h1>Statistics</h1>
+  </div>
+  <div class="row d-flex justify-content-center">
+    <div class="col-lg-4 mb-4">
+      <div class="card text-center">
+        <div class="card-body">
+          <h1 class="card-title">
+            0
+          </h1>
+          <p class="card-text">
+            Statistics 1
+          </p>
         </div>
+      </div>
     </div>
+    <div class="col-lg-4 mb-4">
+      <div class="card text-center">
+        <div class="card-body">
+          <h1 class="card-title">
+            0
+          </h1>
+          <p class="card-text">
+            Statistics 2
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 mb-4">
+      <div class="card text-center">
+        <div class="card-body">
+          <h1 class="card-title">
+            0
+          </h1>
+          <p class="card-text">
+            Statistics 3
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="servers">
+  <div class="d-flex justify-content-center m-2">
+    <h1>Servers</h1>
+  </div>
+  <div class="card mb-3">
+    <div class="card-body">
+      <h5 class="card-title">Server Name</h5>
+      <div class="card-subtitle mb-2">Players: 0/0 | Map: map_here | IP: ip_here</div>
+      <p class="card-text">Description about the server</p>
+      <div class="progress mb-3">
+        <div class="progress-bar progress-bar-striped bg-success progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%">25%</div>
+      </div>
+      <div class="d-grid">
+        <button type="button" class="btn btn-primary">Join Server</button>
+      </div>
+    </div>
+  </div>
+</section>
 @endsection
