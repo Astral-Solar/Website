a8c1787845e97672eb60cbdc82780bbecbb153da
Tomsci <62972629+itstomsci@users.noreply.github.com>
2021-08-20 17:54:50 +0100

    Updated config

--- config.php
+++ config.php
@@ -1,6 +1,12 @@
 <?php
 $config = [];
-
-$config['App Name'] = "App Template";
+// Name of app
+$config['App Name'] = "Astral";
+// Debug
+$config['Debug'] = true;
+// Domain
+$config['Domain'] = "http://localhost";
+// Steam API Key
+$config['SteamAPI Key'] = "BF5F7B41DF9908D97CAFC2EB3DF8C92D";
 
 return $config;
