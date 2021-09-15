8990d423b60dadea75c34a605a02f5f44c01d801
Owain <owain.jones74@gmail.com>
2021-09-15 09:04:31 +0100

    update sample config

--- sample.config.php
+++ sample.config.php
@@ -3,17 +3,95 @@ $config = [];
 // Name of app
 $config['App Name'] = "Astral";
 // Debug
-$config['Debug'] = true;
+$config['Debug'] = false;
 // Domain
-$config['Domain'] = "http://localhost";
+$config['Domain'] = "https://astral.solar";
 // Steam API Key
-$config['SteamAPI Key'] = "BF5F7B41DF9908D97CAFC2EB3DF8C92D";
+$config['SteamAPI Key'] = "KEY";
+// Default usergroup
+$config['Default Usergroup'] = "user";
+// xAdmin prefix for permissions
+$config['xAdmin Permission Prefix'] = "svr1";
+// The largest size a file uploaded by a user can be, in bytes
+$config['File Size'] = 1e+7;
 // Database connection
-$config['Database Creds'] = [
-    'host' => 'localhost',
-    'name' => 'production_db',
-    'user' => 'root',
-    'pass' => ''
+$config['Main Database Credentials'] = [
+    'host' => '1.1.1.1',
+    'name' => 'database',
+    'user' => 'user',
+    'pass' => 'password'
+];
+$config['xAdmin Database Credentials'] = [
+    'host' => '1.1.1.1',
+    'name' => 'database',
+    'user' => 'user',
+    'pass' => 'password'
+];
+// Paddle config
+
+$config['Paddle'] = [
+    'sandbox' => false,
+    'vendorID' => '123456',
+    'APIKey' => 'APIKey',
+    'productID' => '1234' // The premium product subscription ID
+];
+// Permissions
+// The base permission nodes
+$config['Permissions'] = [
+    // Groups
+    [
+        'node' => 'groups.create',
+        'name' => "Create Groups",
+    ],
+    [
+        'node' => 'groups.edit',
+        'name' => "Edit Groups",
+    ],
+    [
+        'node' => 'groups.edit.permissions',
+        'name' => "Edit Group Permissions",
+    ],
+    // Forums
+    [
+        'node' => 'forums.board.create',
+        'name' => "Create Forum Boards",
+    ],
+    [
+        'node' => 'forums.board.delete',
+        'name' => "Delete Forum Boards",
+    ],
+    [
+        'node' => 'forums.board.edit',
+        'name' => "Edit Forum Boards",
+    ],
+    [
+        'node' => 'forums.board.permissions',
+        'name' => "Edit Forum Permissions",
+    ],
+];
+// Forum permission nodes
+$config['Forum Thread Permissions'] = [
+    // Groups
+    [
+        'node' => 'forums.thread.read',
+        'name' => "View Threads",
+    ],
+    [
+        'node' => 'forums.thread.write',
+        'name' => "Create Threads",
+    ],
+    [
+        'node' => 'forums.thread.close',
+        'name' => "Lock Threads",
+    ],
+    [
+        'node' => 'forums.thread.sticky',
+        'name' => "Pin Threads",
+    ],
+    [
+        'node' => 'forums.thread.delete',
+        'name' => "Delete Threads",
+    ]
 ];
 
 return $config;
