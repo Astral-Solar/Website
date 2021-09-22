3fced904a2f28c3f21a55b683e90e5fa53571e06
Owain <owain.jones74@gmail.com>
2021-09-22 14:10:23 +0100

    fix replies giving results in the wrong order

--- class/Thread.php
+++ class/Thread.php
@@ -156,6 +156,7 @@ class Thread
         global $databaseMain;
 
         $results = $databaseMain->from('forums_threads_posts')
+            ->orderBy('created', 'DESC')
             ->where('thread_id')->is($this->GetID())
             ->select()
             ->all();
@@ -175,6 +176,7 @@ class Thread
         global $databaseMain;
 
         $results = $databaseMain->from('forums_threads_posts')
+            ->orderBy('created', 'DESC')
             ->where('thread_id')->is($this->GetID())
             ->select()
             ->first();
