dc4074634cd54808a5de41c9b8969cb754a0e0c7
Owain-Laptop <owain.jones74@gmail.com>
2021-09-28 14:27:49 +0100

    localise all js dependencies
    Also remove some redundant boostrap :omegalul: stuff

--- views/layouts/app.blade.php
+++ views/layouts/app.blade.php
@@ -25,24 +25,16 @@
   <meta property="twitter:description" content>
   <meta property="twitter:image" content>
 
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.js" type="text/javascript"
-          integrity="sha512-n/4gHW3atM3QqRcbCn6ewmpxcLAHGaDjpEBu4xZd47N0W2oQ+6q7oc3PXstrJYXcbNU1OHdQ1T7pAP+gi5Yu8g=="
-          crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-
-  <script src="https://cdn.jsdelivr.net/npm/jquery@3.3.1/dist/jquery.min.js"></script>
+  <script src="{{ $config->get('Domain') }}/public/js/jquery.min.js"></script>
 
   <link rel="stylesheet" type="text/css" href="{{ $config->get('Domain') }}/public/fomantic/semantic.min.css">
   <script src="{{ $config->get('Domain') }}/public/fomantic/semantic.min.js"></script>
 
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lato-font/3.0.0/css/lato-font.css" type="text/css"
-    integrity="sha512-DdjGGRLdxZPin6QfSJ0dhPPTvyFzCvNl7jhl/AKMa3oyfUmelLEFLXi9thQWf3ynI9q/ehTcVPb+o1z8oo5ydg=="
-    crossorigin="anonymous" referrerpolicy="no-referrer">
   <link rel="stylesheet" href="{{ $config->get('Domain') }}/public/css/main.css" type="text/css">
 
-  <script src="//cdn.quilljs.com/1.3.6/quill.js"></script>
-  <script src="/public/js/quill_toolbar.js"></script>
-  <script src="//cdn.quilljs.com/1.3.6/quill.min.js"></script>
-  <link href="//cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
+  <script src="{{ $config->get('Domain') }}/public/js/quill_toolbar.js"></script>
+  <script src="{{ $config->get('Domain') }}/public/js/quill.min.js"></script>
+  <link href="{{ $config->get('Domain') }}/public/js/quill.snow.css" rel="stylesheet">
 
   <script>
     $(document)
